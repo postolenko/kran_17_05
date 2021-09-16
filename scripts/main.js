606da388c08d7c1be4be6da98fc87e77fbd99d89
postolenko <denis.postolenko@gmail.com>
2021-09-16 17:10:07 +0300

    Kran

--- scripts/main.js
+++ scripts/main.js
@@ -263,6 +263,7 @@
       } else {
         $(this).removeClass("error");
         $("#submitBtn").removeClass("cansel");
+        $('#filledInput').val($('#fillingInput').val().slice(0,maxLength));
       }
       console.log($(this).val().length +"   "+ maxLength);
     });
